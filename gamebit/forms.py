584bea324e74b0df5531f4deb8859a7ea3270e2f
Ljubomir <ljubomirvukadin@ssklivno.net>
2022-05-30 09:19:38 +0200

    dodani widgets u forms.py

--- gamebit/forms.py
+++ gamebit/forms.py
@@ -11,4 +11,9 @@ class Reviewform(forms.ModelForm):
             "like": "Do you like this game",
             "dislike": "Do you dislike this game"
         }
-        
+        widgets = {
+            "user_name" : forms.TextInput(attrs={'class': "form__field"}),
+            "text" : forms.TextInput(attrs={'class': "form__field"}),
+            "like" : forms.CheckboxInput(attrs={'class': "boolean"}),
+            "dislike" : forms.CheckboxInput(attrs={'class': "noboolean"}),
+        }
