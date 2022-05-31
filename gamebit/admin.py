b54f840c75570c13173e3f5600c905448564f330
Ljubomir <ljubomirvukadin@ssklivno.net>
2022-05-31 11:35:57 +0200

    povezana Category i gameStudio baza sa admin.py

--- gamebit/admin.py
+++ gamebit/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gamebit.models import Game, Category, gameStudio, Review
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(gameStudio)
