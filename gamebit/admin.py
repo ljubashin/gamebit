c173f99e2392f4038ee70cd56c6d12cef29f5b47
Ljubomir <ljubomirvukadin@ssklivno.net>
2022-05-31 11:37:29 +0200

    dodana baza igara u admin.py

--- gamebit/admin.py
+++ gamebit/admin.py
@@ -3,6 +3,7 @@ from gamebit.models import Game, Category, gameStudio, Review
 
 # Register your models here.
 
+admin.site.register(Game)
 admin.site.register(Category)
 admin.site.register(gameStudio)
 admin.site.register(Review)
